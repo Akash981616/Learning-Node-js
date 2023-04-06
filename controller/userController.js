f0db5bfdd93b9d7302bc9b8f4f3e54405842f77b
Akash <akash98161615@gmail.com>
2023-04-06 23:25:12 +0530

    docker setup done

--- controller/userController.js
+++ controller/userController.js
@@ -62,6 +62,7 @@ const registerUser = asyncHandler(async (req, res) => {
       },
     }),
   };
+
   const user = await User.create(newUser);
 
   if (user) {
@@ -80,37 +81,42 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User not found");
   }
 });
+
 const refreshToken = asyncHandler(async (req, res) => {
- 
- const cookie = req.cookies;
- if(!cookie?.jwt) return res.status(401).json({message:"Unauthorized"})
- const refreshTkn = cookie.jwt
- jwt.verify(refreshTkn, "fasdfas",asyncHandler(async(err,decoded)=>{
-  if(err) res.status(403).json({message :"Forbidden"});
-  const foundUser = await User.findOne({email :decoded.email})
-  if(!foundUser)res.status(401).json({ message: "Unauthorized" });
-   const refreshToken = generateToken({ user }, "fasdfas", {
-     expiresIn: "7d",
-   });
-   res.status(201).json({ refreshToken });
- }));
- 
+  const cookie = req.cookies;
+  if (!cookie?.jwt) return res.status(401).json({ message: "Unauthorized" });
+  const refreshTkn = cookie.jwt;
+  jwt.verify(
+    refreshTkn,
+    "fasdfas",
+    asyncHandler(async (err, decoded) => {
+      if (err) res.status(403).json({ message: "Forbidden" });
+      const foundUser = await User.findOne({ email: decoded.email });
+      if (!foundUser) res.status(401).json({ message: "Unauthorized" });
+      const refreshToken = generateToken({ user }, "fasdfas", {
+        expiresIn: "7d",
+      });
+      res.status(201).json({ refreshToken });
+    })
+  );
 });
+
 //search User
+
 const searchUser = asyncHandler(async (req, res) => {
   const users = await User.find({});
   res.send(users).status(200);
 });
+
 const logOut = asyncHandler(async (req, res) => {
-  
- const cookie = req.cookies;
- if (!cookie?.jwt) return res.status(401).json({ message: "Unauthorized" });
- res.clearCookie("jwt", {
-   httpOnly: true, //accessible only by web server
-   secure: true, //https
-   sameSite: "None", //cross-site cookie
-   maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
- });
-  res.status(200).json({message:"Logout Succesfully" });
+  const cookie = req.cookies;
+  if (!cookie?.jwt) return res.status(401).json({ message: "Unauthorized" });
+  res.clearCookie("jwt", {
+    httpOnly: true, //accessible only by web server
+    secure: true, //https
+    sameSite: "None", //cross-site cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
+  });
+  res.status(200).json({ message: "Logout Succesfully" });
 });
 module.exports = { registerUser, AuthenticateUser, searchUser, refreshToken };
